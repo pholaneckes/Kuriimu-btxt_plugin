03ec0f9e6fe942726f40037d8189339a51979f73
pholaneckes <114641324+pholaneckes@users.noreply.github.com>
2023-08-08 19:23:43 +0800

    Delete src/archive directory

--- src/archive/archive_dpk/DpkSupport.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace archive_dpk.DPK4
-{
-    public class Header
-    {
-        public string Identifier; // DPK4
-    }
-}
--- src/archive/archive_l7c/Compression/DataType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace archive_l7c.Compression
-{
-    public enum DataType
-    {
-        Byte = 1,
-        Short = 2
-    }
-}
